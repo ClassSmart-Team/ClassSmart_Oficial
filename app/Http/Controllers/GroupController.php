44ed57b39bcb38220fbfbc5ae397cadb5c4d33c1
pedro <pedromartell5250@gmail.com>
2026-03-11 22:51:08 -0600

    creaciond de funciones en el controlador GroupController.php

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,40 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\GroupRequest;
+use App\Http\Resources\GroupResource;
 use App\Models\Group;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
+    use ApiResponse;
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-
+        return $this->successResponse(
+            GroupResource::collection(Group::all()),
+            "groups list",
+            200);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create(groupRequest $request)
-    {
 
-        Group::create([
-            'owner'=>$request->user()->id,
-            'period_id'=>$request->period_id,
-            'name'=>$request->name,
-            'description'=>$request->description,
-            'active'=>true,
-        ]);
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(GroupRequest $request)
     {
-        //
+        $group = Group::create([
+            'owner' => $request->user()->id,
+            'period_id' => $request->period_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'active' => true,
+        ]);
+        return $this->successResponse(
+            new GroupResource($group),
+            "group created",
+            201);
+
     }
 
     /**
@@ -44,23 +52,21 @@ class GroupController extends Controller
      */
     public function show(Group $group)
     {
-        //
+        return $this->successResponse(GroupResource::make($group),
+            "group details",
+            200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Group $group)
-    {
-
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Group $group)
+    public function update(GroupRequest $request, Group $group)
     {
-        //
+        $group->update($request->validated());
+        return $this->successResponse(GroupResource::make($group),
+            "group updated",
+            200);
     }
 
     /**
@@ -68,6 +74,8 @@ class GroupController extends Controller
      */
     public function destroy(Group $group)
     {
-        //
+        return $this->successResponse(null,
+            "group deleted",
+            200);
     }
 }
