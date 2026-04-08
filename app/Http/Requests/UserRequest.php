4b63b0b3cb1509b8f02898949112f69617d533bb
sutando <emilianopachecoberlanga@gmail.com>
2026-04-08 14:22:36 -0600

    INTENTO #2

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -23,7 +23,7 @@ class UserRequest extends FormRequest
                 $isUpdate ? 'sometimes' : 'required',
                 'email',
                 'max:255',
-                'unique:users,email,' . $this->route('user'),
+                'unique:users,email,' . $this->route('id'),
             ],
             'password'  => [$isUpdate ? 'sometimes' : 'required', 'string', 'min:6'],
             'cellphone' => ['nullable', 'string', 'max:20'],
