3c79e6cb318cf729b83fd7cc89107a11adc8d57e
sutando <emilianopachecoberlanga@gmail.com>
2026-04-09 13:27:45 -0600

    cambios para tunear el correo, redireccion correcta con cambios en la variable de entorno app.url.front y se añadio la imagen del logo (supuestamente)

--- app/Notifications/VerifyEmailCustom.php
+++ app/Notifications/VerifyEmailCustom.php
@@ -5,6 +5,7 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
 
 class VerifyEmailCustom extends Notification
 {
@@ -19,9 +20,19 @@ class VerifyEmailCustom extends Notification
     {
         return (new MailMessage)
             ->subject('Bienvenido a ClassSmart')
+
             ->greeting('Hola '.$notifiable->name.'!')
+
+            // 👇 IMAGEN AQUÍ
+            ->line(new HtmlString(
+                '<div style="text-align:center; margin-bottom:20px;">
+                    <img src="https://api.sutando-user.me/images/logo-mail.png" width="90" />
+                </div>'
+            ))
+
             ->line('Tu cuenta fue creada correctamente en ClassSmart.')
             ->line('Ya puedes iniciar sesion con tu correo y contraseña.')
-            ->action('Ir a ClassSmart', config('app.url'));
+
+            ->action('Ir a ClassSmart', config('app.url.frontend'));
     }
 }
